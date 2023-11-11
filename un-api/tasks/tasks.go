f6c09a8d9564936d03c2856913b3a8a244e0678e
Uğurcan Akkök <akkokugurcan@gmail.com>
2023-11-11 18:25:41 +0300

    change to 201 code and add data to gettasks

--- un-api/tasks/tasks.go
+++ un-api/tasks/tasks.go
@@ -14,7 +14,12 @@ func GetTasks(c echo.Context) error {
 	c.Logger().Debug("Getting tasks..")
 	tasks := Tasks{
 		Items: []Task{
-			{ID: 1, Status: TaskStatusOpen, Message: "create an awesome task manager app"},
+			{ID: 1, Status: TaskStatusOpen,       Message: "create an awesome task manager app"},
+			{ID: 2, Status: TaskStatusInProgress, Message: "create a beautiful task manager app"},
+			{ID: 3, Status: TaskStatusCompleted,  Message: "create the best task manager app"},
+			{ID: 4, Status: TaskStatusOpen,       Message: "create an intuitive task manager app"},
+			{ID: 5, Status: TaskStatusInProgress, Message: "create a flexible task manager app"},
+			{ID: 6, Status: TaskStatusOpen,       Message: "create a powerful task manager app"},
 		},
 	}
 	return c.JSON(http.StatusOK, tasks)
@@ -27,5 +32,5 @@ func PostTasks(c echo.Context) error {
 		c.Logger().Warn("Cant understand the post data for PostTasks")
 		return c.String(http.StatusBadRequest, "Cant understand post data")
 	}
-	return c.JSON(http.StatusOK, tasks)
+	return c.JSON(http.StatusCreated, tasks)
 }
