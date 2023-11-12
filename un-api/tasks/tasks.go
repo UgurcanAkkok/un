3e73896c3e6e4d38c3c5d5c581c0fe4c6f1cee97
Uğurcan Akkök <akkokugurcan@gmail.com>
2023-11-12 20:07:08 +0300

    format fix

--- un-api/tasks/tasks.go
+++ un-api/tasks/tasks.go
@@ -1,21 +1,21 @@
 package tasks
 
 import (
-	"net/http"
 	"github.com/labstack/echo/v4"
-  un "uakkok.dev/un/common"
+	"net/http"
+	un "uakkok.dev/un/common"
 )
 
 func GetTasks(c echo.Context) error {
 	c.Logger().Debug("Getting tasks..")
 	tasks := un.Tasks{
 		Items: []un.Task{
-			{ID: 1, Status: un.TaskStatusOpen,       Message: "create an awesome task manager app"},
+			{ID: 1, Status: un.TaskStatusOpen, Message: "create an awesome task manager app"},
 			{ID: 2, Status: un.TaskStatusInProgress, Message: "create a beautiful task manager app"},
-			{ID: 3, Status: un.TaskStatusCompleted,  Message: "create the best task manager app"},
-			{ID: 4, Status: un.TaskStatusOpen,       Message: "create an intuitive task manager app"},
+			{ID: 3, Status: un.TaskStatusCompleted, Message: "create the best task manager app"},
+			{ID: 4, Status: un.TaskStatusOpen, Message: "create an intuitive task manager app"},
 			{ID: 5, Status: un.TaskStatusInProgress, Message: "create a flexible task manager app"},
-			{ID: 6, Status: un.TaskStatusOpen,       Message: "create a powerful task manager app"},
+			{ID: 6, Status: un.TaskStatusOpen, Message: "create a powerful task manager app"},
 		},
 	}
 	return c.JSON(http.StatusOK, tasks)
